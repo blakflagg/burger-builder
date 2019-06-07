67d6198a06aa5bf93aa50672efd0026062f9d8d8
Dan Duffy <blakflagg@bitbucket.org>
2019-06-07 16:59:07 -0700

    storing token data in localStorage to save auth state during page reload

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -26,6 +26,9 @@ export const authFail = (error) => {
   };
 };
 export const logout = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('expirationDate');
+  localStorage.removeItem('userId');
   return {
     type: actionTypes.AUTH_LOGOUT
   };
@@ -34,7 +37,7 @@ export const checkAuthTimeout = (expirationTime) => {
   return dispatch => {
     setTimeout(() => {
       dispatch(logout());
-    },expirationTime * 1000)
+    }, expirationTime * 1000)
   };
 };
 export const auth = (email, password, isSignup) => {
@@ -53,6 +56,10 @@ export const auth = (email, password, isSignup) => {
     dispatch(authStart());
     axios.post(url, authData)
       .then(response => {
+        const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+        localStorage.setItem('token', response.data.idToken);
+        localStorage.setItem('expirationDate', expirationDate);
+        localStorage.setItem('userId', response.data.localId);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
         dispatch(checkAuthTimeout(response.data.expiresIn));
       })
@@ -68,3 +75,21 @@ export const setAuthRedirectPath = (path) => {
     path: path
   };
 };
+
+export const authCheckState = () => {
+  return dispatch => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem('expirationDate'));
+      if (expirationDate <= new Date()) {
+        dispatch(logout());
+      } else {
+        const userId = localStorage.getItem('userId');
+        dispatch(authSuccess(token, userId));
+        dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime() / 1000)));
+      }
+    }
+  }
+}
